85f007195693e0c0215ec1fd84b654886231d2f4
youer0219 <xlm20040219@qq.com>
2025-03-29 17:04:47 +0800

    使用vector常量替换原本的重力状态常量

--- components/grivate_area_2d.gd
+++ components/grivate_area_2d.gd
@@ -1,19 +1,19 @@
 extends Area2D
 class_name GravityArea2D
 
-enum GravityState {
-	DOWN = Vector2i.DOWN.y,
-	UP = Vector2i.UP.y,
-}
-
-@export var gravity_state:GravityState = GravityState.DOWN:set = _set_gravity_state
-
-func _set_gravity_state(value:GravityState):
-	gravity_state = value
-	gravity_direction.y = gravity_state
+#enum GravityState {
+	#DOWN = Vector2i.DOWN.y,
+	#UP = Vector2i.UP.y,
+#}
+#
+#@export var gravity_state:GravityState = GravityState.DOWN:set = _set_gravity_state
+#
+#func _set_gravity_state(value:GravityState):
+	#gravity_state = value
+	#gravity_direction.y = gravity_state
 
 func _ready() -> void:
 	EventBus.gravity_flip.connect(
-		func(new_gravity_state:GravityState):
-			gravity_state = new_gravity_state
+		func(new_gravity_direction:Vector2):
+			gravity_direction = new_gravity_direction
 	)
--- scenes/gravity_flip_area_2d/gravity_flip_area_2d.gd
+++ scenes/gravity_flip_area_2d/gravity_flip_area_2d.gd
@@ -1,31 +1,34 @@
+@tool
 extends Area2D
 class_name GravityFlipArea2D
 
 @onready var image: Sprite2D = $Image
 
-@export var gravity_state:GravityArea2D.GravityState = GravityArea2D.GravityState.UP:set = _set_gravity_state
+@export var gravity_state:Vector2 = Vector2.UP:set = _set_gravity_state
 
 const UP_IMAGE_REGION_X := 170
 const DOWN_IMAGE_REGION_X := 187
-#const LEFT_IMAGE_REGION_X := 203
-#const LEFT_IMAGE_REGION_X := 221
+const LEFT_IMAGE_REGION_X := 203
+const RIGHT_IMAGE_REGION_X := 221
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		EventBus.gravity_flip.emit(gravity_state)
 
-func _set_gravity_state(value:GravityArea2D.GravityState):
+func _set_gravity_state(value:Vector2):
 	gravity_state = value
 	
 	if not is_node_ready():
 		await ready
 	
 	match gravity_state:
-		GravityArea2D.GravityState.DOWN:
+		Vector2.DOWN:
 			image.texture.region.position.x = DOWN_IMAGE_REGION_X
-		GravityArea2D.GravityState.UP:
+		Vector2.UP:
 			image.texture.region.position.x = UP_IMAGE_REGION_X
-		#GravityArea2D.GravityState.LEFT:
-			#image.texture.region.position.x = LEFT_IMAGE_REGION_X
-		#GravityArea2D.GravityState.LEFT:
-			#image.texture.region.position.x = LEFT_IMAGE_REGION_X
+		Vector2.LEFT:
+			image.texture.region.position.x = LEFT_IMAGE_REGION_X
+		Vector2.RIGHT:
+			image.texture.region.position.x = RIGHT_IMAGE_REGION_X
+		_:
+			push_error("错误的gravity_state值！")
