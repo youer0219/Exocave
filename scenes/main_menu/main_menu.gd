34198be8d2a023a33f540f1a3568771f126db570
youer0219 <xlm20040219@qq.com>
2025-03-30 21:39:43 +0800

    实现音频单例和音乐播放

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -3,5 +3,12 @@ extends Control
 
 @export var first_level_name:String
 
+@export var title_music:AudioStream
+@export var level_music:AudioStream
+
+func _ready() -> void:
+	MusicPlayer.play(title_music,true,1.0)
+
 func _on_button_pressed() -> void:
 	SceneManager.change_scene_to_level(first_level_name)
+	MusicPlayer.play(level_music,true,1.0)
