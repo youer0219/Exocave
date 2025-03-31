b11da946a5e85b53a762130aa8c7ca101e3d9d73
youer0219 <xlm20040219@qq.com>
2025-03-31 16:30:16 +0800

    关卡五优化：返回时间增长、路线优化、添加误导与提示

--- scenes/player_speed_limit/player_speed_limit.gd
+++ scenes/player_speed_limit/player_speed_limit.gd
@@ -5,5 +5,5 @@ var player_once_entered:bool = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and not player_once_entered:
-		body.velocity *= 0.05
+		body.velocity = Vector2.ZERO
 		player_once_entered = true
