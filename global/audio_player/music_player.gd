873e6a8b3eec80822165029afdc3df4974235c7c
youer0219 <xlm20040219@qq.com>
2025-03-31 17:41:01 +0800

    音乐：临时添加循环播放功能、强制只能同时播放一个音乐

---
+++ global/audio_player/music_player.gd
@@ -0,0 +1,6 @@
+extends "res://global/audio_player/audio_player.gd"
+
+func _on_audio_stream_player_finished() -> void:
+	print($AudioStreamPlayer.playing)
+	$AudioStreamPlayer.play()
+	print("sadasd")
