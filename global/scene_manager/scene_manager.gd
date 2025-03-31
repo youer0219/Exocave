5b774c6039dc9e5152d3af307351915b1867ded1
youer0219 <xlm20040219@qq.com>
2025-03-31 08:42:49 +0800

    添加关卡五

--- global/scene_manager/scene_manager.gd
+++ global/scene_manager/scene_manager.gd
@@ -3,16 +3,10 @@ extends Node
 
 
 @export var levels_dic:Dictionary[String,PackedScene]
-@export var test_level:PackedScene
 
 func change_scene_to_level(level_name:String):
 	if not levels_dic.has(level_name):
 		print("level_name not exit!")
-		return
-	
-	if test_level:
-		get_tree().call_deferred("change_scene_to_packed",test_level)
-		print("test level")
 		return
 	
 	var new_level = levels_dic[level_name]
