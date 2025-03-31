1a1d3cc7fd3ea759d5593357425a38bb5aa278b4
youer0219 <xlm20040219@qq.com>
2025-03-31 21:40:19 +0800

    完成最后一个结局

--- scenes/ends/end.gd
+++ scenes/ends/end.gd
@@ -1,10 +1,10 @@
 extends Control
 
 
-
+@export var restart_level_name:String
 
 func _on_restart_button_pressed() -> void:
-	pass # Replace with function body.
+	SceneManager.change_scene_to_level(restart_level_name)
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
