580a86de94493595b8209d04fdb42c895b6f2f87
youer0219 <xlm20040219@qq.com>
2025-04-06 16:43:41 +0800

    音频播放：设置音乐音量、删除音频总线

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -5,8 +5,12 @@ extends Control
 @export_file var title_music_path:String
 @export_file var level_music_path:String
 
+@export_range(0.0,1.0,0.01) var music_volume := 0.75
+
 func _ready() -> void:
 	CoreSystem.scene_manager.preload_scene(first_level_path)
+	
+	CoreSystem.audio_manager.set_volume(CoreSystem.AudioManager.AudioType.MUSIC,music_volume)
 	CoreSystem.audio_manager.play_music(title_music_path,1.0,false)
 	CoreSystem.audio_manager.preload_audio(level_music_path,CoreSystem.AudioManager.AudioType.MUSIC)
 
